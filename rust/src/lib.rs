ed376a90797ec9bda313992c0877e92d84f7e380
Winrhy <malo@arkada.fr>
2025-01-12 18:41:05 +0100

    feat: :sparkles: Expose check_connectivity

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,2 +1,8 @@
 pub mod api;
 mod frb_generated;
+
+
+#[flutter_rust_bridge::frb]
+pub fn check_connectivity(address: String) -> Result<bool, String> {
+    crate::api::node_management::operations::check_node_connectivity(&address)
+}
